3725d25f2ee579a9ecbcfb10e5b38947854aeb07
Eugene Shmakov <schmak.ekb@gmail.com>
2021-12-09 11:01:54 +0500

    Day 09 part 2 solution

--- src/main/kotlin/day09/Day09.kt
+++ src/main/kotlin/day09/Day09.kt
@@ -14,6 +14,35 @@ fun part1(input: List<List<Int>>): Int =
         }
     }.flatten().sumOf { it + 1 }
 
+fun part2(input: List<List<Int>>): Int {
+    val height = input.size
+    val width = input[0].size
+    val basins = List(height) { MutableList(width) { 0 } }
+    var num = 1
+
+    fun dfs(x: Int, y: Int) {
+        if (x !in 0 until width || y !in 0 until height || input[y][x] > 8 || basins[y][x] > 0) return
+        basins[y][x] = num
+        dfs(x - 1, y)
+        dfs(x + 1, y)
+        dfs(x, y - 1)
+        dfs(x, y + 1)
+    }
+
+    repeat(height) { y ->
+        repeat(width) { x ->
+            dfs(x, y)
+            num++
+        }
+    }
+    return basins.flatten()
+        .filter { it != 0 }
+        .groupingBy { it }.eachCount()
+        .values.sortedDescending()
+        .take(3)
+        .reduce { a, b -> a * b }
+}
+
 private fun List<List<Int>>.getOrMax(x: Int, y: Int) = this.getOrNull(y)?.getOrNull(x) ?: MAX_VALUE
 
 fun parseInput(input: List<String>): List<List<Int>> = input.map { line -> line.map { it.digitToInt() } }
@@ -21,4 +50,5 @@ fun parseInput(input: List<String>): List<List<Int>> = input.map { line -> line.
 fun main() {
     val input = parseInput(readFile("09"))
     println(part1(input))
+    println(part2(input))
 }
--- src/test/kotlin/day09/Day09Test.kt
+++ src/test/kotlin/day09/Day09Test.kt
@@ -11,6 +11,12 @@ internal class Day09Test {
         assertThat(actual).isEqualTo(15)
     }
 
+    @Test
+    fun part2() {
+        val actual = part2(input)
+        assertThat(actual).isEqualTo(1134)
+    }
+
     companion object {
         private val input = parseInput(readFile("09", type = "test"))
     }
