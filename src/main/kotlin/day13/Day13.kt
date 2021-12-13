1f640bd017ce21e72e4cdfa0e81e9a10e854c013
Eugene Shmakov <schmak.ekb@gmail.com>
2021-12-13 10:37:33 +0500

    Day 13 part 2 solution

--- src/main/kotlin/day13/Day13.kt
+++ src/main/kotlin/day13/Day13.kt
@@ -7,7 +7,18 @@ fun part1(input: Input): Int {
     return instruction.apply(input.dots).size
 }
 
+fun part2(input: Input) {
+    val dots = input.instructions.fold(input.dots) { dots, instruction -> instruction.apply(dots) }
+    val width = dots.maxOf { it.x } + 1
+    val height = dots.maxOf { it.y } + 1
+    val result = List(height) { MutableList(width) { '.' } }
+
+    dots.forEach { (x, y) -> result[y][x] = '#' }
+    result.forEach { println(it.joinToString("")) }
+}
+
 fun main() {
     val input = readFile("13")
     println(part1(Input.parse(input)))
+    part2(Input.parse(input))
 }
