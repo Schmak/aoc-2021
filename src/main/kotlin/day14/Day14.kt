0cd13f8f47606aa823280678d90a0081b8853d37
Eugene Shmakov <schmak.ekb@gmail.com>
2021-12-14 11:23:03 +0500

    Day 14 part 2 solution

--- src/main/kotlin/day14/Day14.kt
+++ src/main/kotlin/day14/Day14.kt
@@ -4,7 +4,10 @@ import utils.readFile
 
 fun part1(input: Input): Long = Mutator(input).calculate(10)
 
+fun part2(input: Input): Long = Mutator(input).calculate(40)
+
 fun main() {
     val input = Input.parse(readFile("14"))
     println(part1(input))
+    println(part2(input))
 }
--- src/test/kotlin/day14/Day14Test.kt
+++ src/test/kotlin/day14/Day14Test.kt
@@ -11,6 +11,12 @@ internal class Day14Test {
         assertThat(actual).isEqualTo(1588)
     }
 
+    @Test
+    fun part2() {
+        val actual = part2(input)
+        assertThat(actual).isEqualTo(2188189693529)
+    }
+
     companion object {
         private val input = Input.parse(readFile("14", type = "test"))
     }
